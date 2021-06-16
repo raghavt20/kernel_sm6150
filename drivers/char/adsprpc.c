062c2aa6dc414d90c22cbc38165338ddb28e22ca
JJ Lee <leejj@google.com>
2021-06-16 10:03:08 +0800

    char: adsprpc: fix global-out-of-bounds error
    The array fastrpc_rpmsg_match is not ended with {}, out-of-bounds access
    might occur when enumerating the ids listed in fastrpc_rpmsg_match.
    Added {} to prevent out-of-bounds access.
    
    bug: 189164670
    Change-Id: Ia18e70c7739fca242c70187083c8fc651f98e968
    Signed-off-by: JJ Lee <leejj@google.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -4738,6 +4738,7 @@ static struct platform_driver fastrpc_driver = {
 static const struct rpmsg_device_id fastrpc_rpmsg_match[] = {
 	{ FASTRPC_GLINK_GUID },
 	{ FASTRPC_SMD_GUID },
+	{},
 };
 
 static const struct of_device_id fastrpc_rpmsg_of_match[] = {
